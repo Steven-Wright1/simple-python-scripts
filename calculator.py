659758905fc5c6133a0888dd83070dc9b78019ee
Steven-Wright1 <steven.wright593@gmail.com>
2024-10-26 16:18:40 +0100

    commiting changes to include input validation for operand

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-
-def additon(x,y):
+def check_valid_operand(operand):
+        valid_operands = ["+","-","*","/"]
+        while operand not in valid_operands:
+            operand = input("please enter valid operand - one of + - * or /\n")
+        return operand
+        
+def addition(x,y):
     return x + y
 
 def subtraction(x,y):
@@ -18,22 +23,23 @@ def main():
 
     print("##########################################\n######## Simple Python Calculator ########\n##########################################\n\n") 
 
+
     operand = input("Specify the operand as one of + - / *\n")
-    
+    operand = check_valid_operand(operand)
+        
     num1 = float(input("Specify the first number: "))
     num2 = float(input("specify the second number: "))
 
     if (operand == "+"):
-        print(f"The answer is {additon(num1,num2):g}")
+        print(f"The answer is {addition(num1,num2):g}")
     elif (operand == "-"):
         print(f"The answer is {subtraction(num1,num2):g}")
-    if (operand == "*"):
+    elif (operand == "*"):
         print(f"The answer is {multiplication(num1,num2):g}")
-    if (operand == "/"):
+    elif (operand == "/"):
         print(f"The answer is {division(num1,num2):g}")
+
  
-
-
 
 if __name__ == "__main__":
     main()
