a95ae7c28926429cec2903b077517f8b884bd9a9
Steven-Wright1 <steven.wright593@gmail.com>
2024-11-05 20:23:27 +0000

    functions attached to buttons, perform basic calc functions. Next steps - error handling and friendly display of / and *

--- calculator.py
+++ calculator.py
@@ -5,7 +5,23 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def calculation(result):
+    result["text"] = eval("".join(sequence))
+    del sequence[:]
+
+def expression(result, value):
+    sequence.append(value)
+    result["text"]="".join(sequence)
+
+def clear(result):
+    del sequence[:]
+    result["text"] = ""
+
 def main():
+
+    global sequence
+    sequence = []
 
     root=tk.Tk()
     root.title("Simple Python Calculator")
@@ -16,44 +32,44 @@ def main():
     style.configure('cancel.TButton',background='brown3',padding=15)
 
 
-    result = tk.Label(root,text="Display Field for result",background='white',padx=150,pady=20)
+    result = tk.Label(root,text="Display field for result",background='white',padx=150,pady=20)
     result.grid(row=1,columnspan=4,padx=10,pady=20, sticky='nsew')
 
-    button1 = ttk.Button(root,text="7",style='TButton')
+    button1 = ttk.Button(root,text="7",style='TButton', command=lambda: expression(result,'7'))
     button1.grid(row=2, column=0,pady=5)
-    button2 = ttk.Button(root,text="8",style='TButton')
+    button2 = ttk.Button(root,text="8",style='TButton',command=lambda: expression(result,'8'))
     button2.grid(row=2,column=1)
-    button3 = ttk.Button(root,text="9",style='TButton')
+    button3 = ttk.Button(root,text="9",style='TButton', command=lambda: expression(result, '9'))
     button3.grid(row=2,column=2)
-    button4 = ttk.Button(root,text="+",style='TButton')
+    button4 = ttk.Button(root,text="+",style='TButton',command=lambda: expression(result,'+'))
     button4.grid(row=2,column=3)
 
 
-    button5 = ttk.Button(root,text="4",style='TButton')
+    button5 = ttk.Button(root,text="4",style='TButton', command=lambda: expression (result, '4'))
     button5.grid(row=3,column=0,pady=5)
-    button6 = ttk.Button(root,text="5",style='TButton')
+    button6 = ttk.Button(root,text="5",style='TButton', command=lambda: expression(result,'5'))
     button6.grid(row=3,column=1)
-    button7 = ttk.Button(root,text="6",style='TButton')
+    button7 = ttk.Button(root,text="6",style='TButton', command=lambda: expression(result, '6'))
     button7.grid(row=3,column=2)
-    button8 = ttk.Button(root,text="-",style='TButton')
+    button8 = ttk.Button(root,text="-",style='TButton', command=lambda: expression(result, '-'))
     button8.grid(row=3,column=3)
 
-    button9 = ttk.Button(root,text="1",style='TButton')
+    button9 = ttk.Button(root,text="1",style='TButton', command=lambda: expression(result, '1'))
     button9.grid(row=4,column=0,pady=5)
-    button10 = ttk.Button(root,text="2",style='TButton')
+    button10 = ttk.Button(root,text="2",style='TButton', command=lambda: expression(result, '2'))
     button10.grid(row=4,column=1)
-    button11 = ttk.Button(root,text="3",style='TButton')
+    button11 = ttk.Button(root,text="3",style='TButton', command=lambda: expression(result, '3'))
     button11.grid(row=4,column=2)
-    button12 = ttk.Button(root,text="x",style='TButton')
+    button12 = ttk.Button(root,text="x",style='TButton', command=lambda: expression(result, '*'))
     button12.grid(row=4,column=3)
     
-    button13 = ttk.Button(root,text="C",style='cancel.TButton')
+    button13 = ttk.Button(root,text="C",style='cancel.TButton', command=lambda: clear(result))
     button13.grid(row=5,column=0,pady=5)
-    button14 = ttk.Button(root,text="0",style='TButton')
+    button14 = ttk.Button(root,text="0",style='TButton', command=lambda: expression(result, '0'))
     button14.grid(row=5,column=1)
-    button15 = ttk.Button(root,text="=",style='TButton')
+    button15 = ttk.Button(root,text="=",style='TButton', command=lambda: calculation(result))
     button15.grid(row=5,column=2)
-    button16 = ttk.Button(root,text="÷",style='TButton')
+    button16 = ttk.Button(root,text="÷",style='TButton', command=lambda: expression(result, '/'))
     button16.grid(row=5,column=3)
 
 
